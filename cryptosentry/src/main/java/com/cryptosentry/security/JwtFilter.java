d4e1ea426a3cf2fb745a4c64d1f8095b73d58168
Jhonatan Willian <jhonatanwillian2006@gmail.com>
2026-03-29 17:12:43 -0300

    feat: adiciona autenticação JWT com filtro e util

--- cryptosentry/src/main/java/com/cryptosentry/security/JwtFilter.java
+++ cryptosentry/src/main/java/com/cryptosentry/security/JwtFilter.java
@@ -1,7 +1,78 @@
 package com.cryptosentry.security;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
-public class JwtFilter {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collections;
 
 
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+
+        final String authHeader = request.getHeader("Authorization"); // pega o header
+
+        String token = null;
+        String username = null;
+
+
+                       // extrai o token
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            token = authHeader.substring(7);
+
+            try {
+                username = jwtUtil.extractUsername(token); // extrai o username do token
+            } catch (Exception e) {
+                System.out.println("token invalido");
+            }
+        }
+
+
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+
+
+                 // verifica se não expirou
+            if (!jwtUtil.isTokenExpired(token)) {
+
+
+                         // cria autenticação
+                UsernamePasswordAuthenticationToken authToken =
+                        new UsernamePasswordAuthenticationToken(
+                                username,
+                                null,
+                                Collections.emptyList()
+                        );
+
+                                   // adiciona detalhes da request
+                authToken.setDetails(
+                        new WebAuthenticationDetailsSource().buildDetails(request)
+                );
+
+                                   // autentica no Spring
+                SecurityContextHolder.getContext().setAuthentication(authToken); /
+            }
+        }
+
+
+        filterChain.doFilter(request, response); // continua a requisição
+    }
 }
--- cryptosentry/src/main/java/com/cryptosentry/security/JwtUtil.java
+++ cryptosentry/src/main/java/com/cryptosentry/security/JwtUtil.java
@@ -1,7 +1,52 @@
 package com.cryptosentry.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
 
+import java.util.Date;
+
+@Component
 public class JwtUtil {
 
+    private final String SECRET_KEY = "minha_chave_secreta";
 
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))//24h
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    private Claims extractClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody();
+
+    }
+                //  Extrair username
+    public String extractUsername(String token) {
+        return extractClaims(token).getSubject();
+    }
+               //  Verificar se expirou
+    public boolean isTokenExpired(String token) {
+        return extractClaims(token).getExpiration().before(new Date());
+    }
+               //  Validar token
+    public boolean validateToken(String token, String username) {
+        String extractedUsername = extractUsername(token);
+        return (extractedUsername.equals(username)) && !isTokenExpired(token);
+
+    }
+             //extrair tudo do token
+    private Claims extracted(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody();
+    }
 }
